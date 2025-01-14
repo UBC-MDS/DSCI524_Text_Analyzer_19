cc31b51ad24497fbdaa331d39a853733b7833731
“colombe-tolokin” <colombe.tolokin@outlook.com>
2025-01-14 10:01:18 -0800

    test functions for detect and visualize

---
+++ tests/test_visualize_language_patterns.py
@@ -0,0 +1,7 @@
+def test_visualize_language_patterns(patterns, method="language"):
+    """_summary_
+
+    Args:
+        patterns (_type_): _description_
+        method (str, optional): _description_. Defaults to "language".
+    """
